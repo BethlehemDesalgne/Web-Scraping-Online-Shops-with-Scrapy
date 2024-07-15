bb1102dc16d0b8e78429165824444b53a0ed9e98
Bethlehem Desalgne <114709953+BethlehemDesalgne@users.noreply.github.com>
2024-07-15 19:18:51 -0400

    Update middlewares.py

--- middlewares.py
+++ middlewares.py
@@ -1,7 +1,4 @@
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# Define here the models for spider middleware
 
 from scrapy import signals
 
